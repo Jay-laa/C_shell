f882ee82dc24ff75a076b7c112ab831d91e3fda4
Jay-laa <jporsh78@gmail.com>
2023-04-14 06:42:44 -0700

    initial.commit

---
+++ error_utils.c
@@ -0,0 +1,138 @@
+#include "myshell.h"
+
+/**
+ * err_to_integer - converts a string to an integer with error checking
+ * @s: the string to be converted
+ * Return: 0 if no numbers in string, converted number otherwise
+ *       -1 on error
+ */
+int err_to_integer(char *s)
+{
+	int idx = 0;
+	unsigned long int result = 0;
+
+	if (*s == '+')
+		s++;  /* why does this make main return 255? */
+	for (idx = 0;  s[idx] != '\0'; idx++)
+	{
+		if (s[idx] >= '0' && s[idx] <= '9')
+		{
+			result *= 10;
+			result += (s[idx] - '0');
+			if (result > INT_MAX)
+				return (-1);
+		}
+		else
+			return (-1);
+	}
+	return (result);
+}
+
+/**
+ * print_error - prints an error message to standard error
+ * @info: a pointer to the parameter and return information structure
+ * @error_type: string containing error type to be printed
+ * Return: void
+ */
+void print_error(info_t *info, char *error_type)
+{
+	_eputs(info->filename);
+	_eputs(": ");
+	print_d(info->line_number, STDERR_FILENO);
+	_eputs(": ");
+	_eputs(info->argv[0]);
+	_eputs(": ");
+	_eputs(error_type);
+}
+
+/**
+ * print_digits - function prints a decimal (integer) number (base 10)
+ * @num: the number to print
+ * @fd: the file descriptor to write to
+ *
+ * Return: number of characters printed
+ */
+int print_digits(int num, int fd)
+{
+	int (*__putchar)(char) = _putchar;
+	int i, char_count = 0;
+	unsigned int abs_num, current;
+
+	if (fd == STDERR_FILENO)
+		__putchar = _eputchar;
+	if (num < 0)
+	{
+		abs_num = -num;
+		__putchar('-');
+		char_count++;
+	}
+	else
+		abs_num = num;
+	current = abs_num;
+	for (i = 1000000000; i > 1; i /= 10)
+	{
+		if (abs_num / i)
+		{
+			__putchar('0' + current / i);
+			char_count++;
+		}
+		current %= i;
+	}
+	__putchar('0' + current);
+	char_count++;
+
+	return (char_count);
+}
+/**
+ * to_string- converts a number to a string in the specified base
+ * @n: the number to convert
+ * @b: the base to use for conversion
+ * @f: flags to modify conversion behavior
+ *
+ * Return: pointer to resulting string
+ */
+char *to_string(long int n, int b, int f)
+{
+	static char *a;
+	static char buf[50];
+	char s = 0;
+	char *p;
+	unsigned long num = n;
+
+	if (!(f & CONVERT_UNSIGNED) && n < 0)
+	{
+		num = -n;
+		s = '-';
+	}
+	a = f & CONVERT_LOWERCASE ? "0123456789abcdef" : "0123456789ABCDEF";
+	p = &buf[49];
+	*p = '\0';
+
+	do {
+		*--p = a[num % b];
+		num /= b;
+	} while (num != 0);
+
+	if (s)
+		*--p = s;
+
+	return (p);
+}
+
+/**
+ * remove_comments - function replaces first instance of '#' with '\0'
+ * @str: address of the string to modify
+ *
+ * Return: Always 0;
+ */
+void remove_comments(char *str)
+{
+	int idx;
+
+	for (idx = 0; str[idx] != '\0'; idx++)
+	if (str[idx] == '#' && (!idx || str[idx - 1] == ' '))
+	{
+		str[idx] = '\0';
+		break;
+	}
+}
---
+++ shellbuiltin1.c
@@ -0,0 +1,117 @@
+#include "'myshell.h"
+
+/**
+ * show_history - prints the history list, one command by line, preceded
+ * with line numbers, starting at 0.
+ * @info: Structure containing potential arguments.
+ *
+ * Return: Always 0
+ */
+int show_history(info_t *info)
+{
+    /* Call print_list to display the history */
+	print_list(info->history);
+	return (0);
+}
+
+/**
+ * unset_alias - removes the alias associated with the given string
+ * @info: structure containing potential arguments. Used to maintain
+ * constant function prototype.
+ * @str: the string representing the alias
+ *
+ * Return: 0 on success, 1 on error
+ */
+int unset_alias(info_t *info, char *str)
+{
+	char *equal_sign_pos, temp;
+	int ret;
+
+	equal_sign_pos = _strchr(str, '=');
+	if (!equal_sign_pos)
+		return (1);
+	temp = *equal_sign_pos;
+	*equal_sign_pos = 0;
+	ret = delete_node_at_index(&(info->alias),
+		get_node_index(info->alias, node_starts_with(info->alias, str, -1)));
+	*equal_sign_pos = temp;
+	return (ret);
+}
+
+/**
+ * set_alias - sets alias to string
+ * @info: structure containing potential arguments
+ * @alias_str: the string alias
+ *
+ * Return: Always 0 on success, 1 on error
+ */
+int set_alias(info_t *info, char *alias_str)
+{
+	char *equal_sign_pos;
+
+	equal_sign_pos = _strchr(alias_str, '=');
+	if (!equal_sign_pos)
+		return (1);
+	if (!*++equal_sign_pos)
+		return (unset_alias(info, alias_str));
+
+	unset_alias(info, alias_str);
+	return (add_node_end(&(info->alias), alias_str, 0) == NULL);
+}
+
+/**
+ * print_alias - prints an alias string
+ * @alias_node: the alias node
+ *
+ * Return: Always 0 on success, 1 on error
+ */
+int print_alias(list_t *alias_node)
+{
+	char *equal_sign_pos = NULL, *alias_str = NULL;
+
+	if (alias_node)
+	{
+		equal_sign_pos = _strchr(alias_node->str, '=');
+		for (alias_str = alias_node->str; alias_str <= equal_sign_pos; alias_str++)
+			_putchar(*alias_str);
+		_putchar('\'');
+		_puts(equal_sign_pos + 1);
+		_puts("'\n");
+		return (0);
+	}
+	return (1);
+}
+
+/**
+ * alias_builtin - mimics the alias builtin (man alias)
+ * @info: Structure containing potential arguments. Used to maintain
+ *        constant function prototype.
+ * Return: Always 0
+ */
+int alias_builtin(info_t *info)
+{
+	int i = 0;
+	char *p = NULL;
+	list_t *node = NULL;
+
+	if (info->argc == 1)
+	{
+		node = info->alias;
+		while (node)
+		{
+			print_alias(node);
+			node = node->next;
+		}
+		return (0);
+	}
+	for (i = 1; info->argv[i]; i++)
+	{
+		p = _strchr(info->argv[i], '=');
+		if (p)
+			set_alias(info, info->argv[i]);
+		else
+			print_alias(node_starts_with(info->alias, info->argv[i], '='));
+	}
+
+	return (0);
+}
