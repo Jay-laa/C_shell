b7c3b3a591b1d3a4993db0594c722071f1165924
Mudiajohn <mudiajohn@yahoo.com>
2023-04-14 11:50:49 +0100

    memory.c

--- memory.c
+++ memory.c
@@ -1,11 +1,11 @@
 #include "myshell.h"
 
 /**
- * free_ptr - frees a pointer and NULLs the address
- * @p: address of the pointer to free
- *
- * Return: 1 if freed, otherwise 0.
- */
+* free_ptr - frees a pointer and NULLs the address
+* @p: address of the pointer to free
+*
+* Return: 1 if freed, otherwise 0.
+*/
 int free_ptr(void **p)
 {
 	if (p && *p)
