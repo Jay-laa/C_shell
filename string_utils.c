5868396a6f68ea3878436d23048de68f1c7219f3
Mudiajohn <mudiajohn@yahoo.com>
2023-04-26 12:30:16 +0100

    string_utils.c

--- string_utils.c
+++ string_utils.c
@@ -1,11 +1,10 @@
 #include "myshell.h"
 
 /**
- * str_length - returns the length of a string
- * @str: the string whose length to check
- *
- * Return: integer length of string
- */
+* str_length - returns the length of a string
+* @str: the string whose length to check
+* Return: integer length of string
+*/
 int str_length(char *str)
 {
 	int len = 0;
@@ -20,13 +19,12 @@ int str_length(char *str)
 }
 
 /**
- * _strcmp - performs lexicographic comparison of two strings.
- * @str1: the first string
- * @str2: the second string
- *
- * Return: negative value if str1 < str2,
- * positive value if str1 > str2, 0 if str1 == str2
- */
+* _strcmp - performs lexicographic comparison of two strings.
+* @str1: the first string
+* @str2: the second string
+* Return: negative value if str1 < str2,
+* positive value if str1 > str2, 0 if str1 == str2
+*/
 int _strcmp(char *str1, char *str2)
 {
 	while (*str1 && *str2)
@@ -43,12 +41,11 @@ int _strcmp(char *str1, char *str2)
 }
 
 /**
- * starts_with - checks if needle starts with prefix
- * @prefix: string to search
- * @needle: the substring to find
- *
- * Return: address of next char of prefix or NULL
- */
+* starts_with - checks if needle starts with prefix
+* @prefix: string to search
+* @needle: the substring to find
+* Return: address of next char of prefix or NULL
+*/
 char *starts_with(const char *prefix, const char *needle)
 {
 	while (*needle)
@@ -58,12 +55,11 @@ char *starts_with(const char *prefix, const char *needle)
 }
 
 /**
- * string_concat - concatenates two strings
- * @str1: the first string
- * @str2: the second string
- *
- * Return: pointer to concatenated string
- */
+* string_concat - concatenates two strings
+* @str1: the first string
+* @str2: the second string
+* Return: pointer to concatenated string
+*/
 char *string_concat(char *str1, char *str2)
 {
 	char *ret = str1;
