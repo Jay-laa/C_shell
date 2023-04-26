94acdc274d5d3b8551c29eb33fe43f66db75dcad
Mudiajohn <mudiajohn@yahoo.com>
2023-04-26 11:57:21 +0100

    command_parser.c

--- command_parser.c
+++ command_parser.c
@@ -1,12 +1,11 @@
 #include "myshell.h"
 
 /**
- * is_command - Determines if a file is an executable command
- * @info: Pointer to the info struct containing command info
- * @path: Path to the file to check
- *
- * Return: 1 if the file is an executable command, 0 otherwise
- */
+* is_command - Determines if a file is an executable command
+* @info: Pointer to the info struct containing command info
+* @path: Path to the file to check
+* Return: 1 if the file is an executable command, 0 otherwise
+*/
 
 int is_command(info_t *info, char *path)
 {
@@ -25,13 +24,12 @@ int is_command(info_t *info, char *path)
 }
 
 /**
- * duplicate_chars - duplicates characters from a string
- * @str: the string to copy characters from
- * @start: the index to start copying from
- * @stop: the index to stop copying at
- *
- * Return: pointer to new buffer
- */
+* duplicate_chars - duplicates characters from a string
+* @str: the string to copy characters from
+* @start: the index to start copying from
+* @stop: the index to stop copying at
+* Return: pointer to new buffer
+*/
 char *duplicate_chars(char *str, int start, int stop)
 {
 	static char buffer[1024];
@@ -50,13 +48,12 @@ char *duplicate_chars(char *str, int start, int stop)
 }
 
 /**
- * find_command_path - finds the full path of a command in the PATH string
- * @info: the info struct
- * @path_str: the PATH string
- * @cmd: the command to find
- *
- * Return: the full path of cmd if found or NULL
- */
+* find_command_path - finds the full path of a command in the PATH string
+* @info: the info struct
+* @path_str: the PATH string
+* @cmd: the command to find
+* Return: the full path of cmd if found or NULL
+*/
 char *find_command_path(info_t *info, char *path_str, char *cmd)
 {
 	int index = 0, start_pos = 0;
